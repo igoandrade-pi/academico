18d11e134ad11057002f08ef6b073a943cc90693
Igo da Costa Andrade <igoandrade.pi@gmail.com>
2023-11-10 12:02:46 -0300

    Criação do modelo para Matrícula

--- edu/models.py
+++ edu/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Matricula(models.Model):
+    matricula = models.IntegerField()
+    nome_aluno = models.CharField(max_length=200)
+    curso = models.CharField(max_length=100)
+    observacao = models.TextField()
+    data_matricula = models.DateTimeField(verbose_name='Data de matrícula', blank=True, auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Matrícula de Aluno'
+        verbose_name_plural = 'Matrículas de Alunos'
+        ordering = ['nome_aluno']
